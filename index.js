fd9ffe3f46b70504936b44538118b0532ef6b3df
Thiago Colebrusco <thiagocolebrusco@gmail.com>
2020-10-03 08:41:42 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require('path');
+const port = 8080
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get("/usuarios", (req, res) => {
+    let usuarios = [
+        {
+            id: 1,
+            nome: "Thiago",
+            email: "thiago@teste.com.br"
+        },
+        {
+            id: 2,
+            nome: "Thiago 2",
+            email: "thiago2@teste.com.br"
+        },
+    ]
+    res.render("lista_usuarios", {usuarios})
+})
+
+app.listen(port, err => {
+    console.log(`Aplicação rodando na porta ${port}`);
+});
