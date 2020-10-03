d561c059c7ad16b891f20db300e206a61475507c
Thiago Colebrusco <thiagocolebrusco@gmail.com>
2020-10-03 14:20:02 -0300

    Trocar a pasta views para dentro de src

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 8080
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
 
